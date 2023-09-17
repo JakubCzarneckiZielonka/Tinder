98dbcc235ca247c975052a396d9ee4a5d32267c4
jc <jakubczarnecki@poczta.onet.pl>
2023-09-17 21:36:09 +0200

    Login and register

--- app/src/main/java/com/example/tinder/Choose_Login_And_Reg.java
+++ app/src/main/java/com/example/tinder/Choose_Login_And_Reg.java
@@ -2,13 +2,36 @@ package com.example.tinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Choose_Login_And_Reg extends AppCompatActivity {
 
+    private Button mLogin, mRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_and_reg);
+
+        mLogin = findViewById(R.id.login);
+        mRegister = findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Choose_Login_And_Reg.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Choose_Login_And_Reg.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
