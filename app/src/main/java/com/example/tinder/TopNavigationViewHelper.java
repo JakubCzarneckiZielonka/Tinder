24562f39e2dc7a5ff32e87d51e5b346d060fcc0f
jc <jakubczarnecki@poczta.onet.pl>
2023-09-20 17:47:38 +0200

    Top Navigation View Helper

---
+++ app/src/main/java/com/example/tinder/TopNavigationViewHelper.java
@@ -0,0 +1,36 @@
+package com.example.tinder;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class TopNavigationViewHelper {
+    private static final String TAG = "TopNavigationViewHelper";
+
+    public static void setupTopNavigationView(BottomNavigationViewEx tv) {
+        Log.d(TAG, "setupTopNavigationView: setting up navigationview");
+    }
+
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view) {
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.ic_profile){
+                    Intent intent = new Intent(context, SettingsActivity.class);
+                    context.startActivity(intent);
+                } else if (itemId == R.id.ic_matched){
+                    Intent intent = new Intent(context, MatchesActivity.class);
+                    context.startActivity(intent);
+                }
+                return false;
+            }
+        });
+    }
+}
