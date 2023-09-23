ab52f3b42f0416388d58c811c08b375aa3e9e41c
jc <jakubczarnecki@poczta.onet.pl>
2023-09-23 18:28:04 +0200

    Cards & Item XML

---
+++ app/src/main/java/com/example/tinder/Cards/arrayAdapter.java
@@ -0,0 +1,90 @@
+package com.example.tinder.Cards;
+
+import static androidx.core.content.ContextCompat.getDrawable;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.example.tinder.R;
+
+import java.util.List;
+
+public class arrayAdapter extends ArrayAdapter<Cards> {
+
+    Context context;
+
+    public arrayAdapter(Context context, int resourceId, List<Cards> items){
+        super(context, resourceId, items);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        Cards card_item = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+        }
+
+        TextView name = convertView.findViewById(R.id.name);
+        ImageView imageView = convertView.findViewById(R.id.image);
+        TextView budget = convertView.findViewById(R.id.budget);
+        ImageView mNeedImage = convertView.findViewById(R.id.needImage);
+        ImageView mGiveImage = convertView.findViewById(R.id.giveImage);
+
+        name.setText(card_item.getName());
+        budget.setText(card_item.getBudget());
+
+        //need image
+        if (card_item.getNeed().equals("Netflix")) {
+            mNeedImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.netflix));
+        } else if (card_item.getNeed().equals("Hulu")) {
+            mNeedImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.hulu));
+        } else if (card_item.getNeed().equals("Vudu")) {
+            mNeedImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.vudu));
+        } else if (card_item.getNeed().equals("HBO Now")) {
+            mNeedImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.hbo));
+        } else if (card_item.getNeed().equals("Youtube Originals")) {
+            mNeedImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.youtube));
+        } else {
+            mNeedImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.none));
+        }
+
+        //give Image
+        if (card_item.getGive().equals("Netflix")) {
+            mGiveImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.netflix));
+        } else if (card_item.getGive().equals("Amazon Prime")) {
+            mGiveImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.amazon));
+        } else if (card_item.getGive().equals("Hulu")) {
+            mGiveImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.hulu));
+        } else if (card_item.getGive().equals("Vudu")) {
+            mGiveImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.vudu));
+        } else if (card_item.getGive().equals("HBO Now")) {
+            mGiveImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.hbo));
+        } else if (card_item.getGive().equals("Youtube Originals")) {
+            mGiveImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.youtube));
+        } else {
+            mGiveImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.none));
+        }
+
+        switch (card_item.getProfileImageUrl()){
+            case "default":
+                Glide.with(convertView.getContext()).load(R.drawable.profile).into(imageView);
+                break;
+            default:
+                Glide.clear(imageView);
+                Glide.with(convertView.getContext()).load(card_item.getProfileImageUrl()).into(imageView);
+                break;
+        }
+        return convertView;
+
+    }
+}
+
--- app/src/main/java/com/example/tinder/SettingsActivity.java
+++ app/src/main/java/com/example/tinder/SettingsActivity.java
@@ -2,13 +2,61 @@ package com.example.tinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private EditText mNameField, mPhoneField;
+    private ProgressBar spinner;
+    private Button mConfirm;
+    private ImageButton mBack;
+    private ImageView mProgileImage;
+    private EditText mBudget;
+    private Spinner need, give;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mUserDatabase;
+    private String userId, name, phone, profileImageUrl, userSex, UserBudget, UserNeed,
+    private int needIndex, giveIndex;
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        spinner = findViewById(R.id.pBar);
+        spinner.setVisibility(View.GONE);
+
+        mNameField = findViewById(R.id.name);
+        mPhoneField = findViewById(R.id.phone);
+
+        mProgileImage = findViewById(R.id.profileImage);
+        mBack = findViewById(R.id.settingsBack);
+
+        mConfirm = findViewById(R.id.confirm);
+        mBudget = findViewById(R.id.budget_settings);
+        need = findViewById(R.id.spinner_need_settings);
+        give = findViewById(R.id.spinner_give_setting);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth != null && mAuth.getCurrentUser() != null)
+            userId = mAuth.getCurrentUser().getUid();
+        else  {
+            finish();
+
+        }
+        
     }
 }
